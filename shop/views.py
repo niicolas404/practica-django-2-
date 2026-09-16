6c4b58acb8edc14ceb59edb9d7ccf1f57f56d8db
niicolas404 <nicolas.jara53@inacapmail.cl>
2026-09-16 00:39:17 -0300

    segundo commit (creación de templates y vistas)

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def detalle(request):
+    return render(request, 'detalle.html')
